17412f89f0c18c55e6d2f0729b13eb500ca51c1b
Bruno Laria <137282287+Bubu90@users.noreply.github.com>
2025-01-20 10:21:52 +0100

    Aggiornamento 2.4
    Cambio titolo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = ({ setMenuOpenForm, menuOpenForm }) => {
                 animate="visible"
                 className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
               >
-                Front end developer
+                Full-Stak Developer
               </motion.span>
               <motion.p
                 variants={container(1)}
